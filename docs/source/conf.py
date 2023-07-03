496bc27a6731d62a4a6a00feac4ec9c257eaa37d
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-07-03 18:23:48 +0100

    Updated sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'm2r',
-]
+extensions = []
 
 # source_suffix = '.rst'
 source_suffix = ['.rst', '.md']
